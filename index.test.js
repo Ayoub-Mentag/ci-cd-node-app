760dbf7e7e378e2b6fc6d9a6c5ad9bb2ce708009
Ayoub-Mentag <ayoubmentag21@gmail.com>
2025-05-07 11:18:18 +0100

    CI/CD

--- index.test.js
+++ index.test.js
@@ -3,7 +3,7 @@ const express = require('express');
 
 const app = express();
 app.get('/health', (req, res) => {
-  res.status(400).send({ status: 'UP' });
+  res.status(200).send({ status: 'UP' });
 });
 
 describe('GET /health', () => {
