512782777eef248e45a832ac15ddf50bde93725f
Ayoub-Mentag <ayoubmentag21@gmail.com>
2025-05-08 12:41:00 +0100

    CI/CD

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/health', (req, res) => {
-  res.status(200).send({ status: 'New version ...' });
+  res.status(200).send({ status: 'New version ... |||| ' });
 });
 
 app.listen(port, () => {
